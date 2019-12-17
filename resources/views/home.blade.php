26e8804aa9fb8abda21a605af085e112c976a4f4
Miguel Marquez <miguelxmarquez@gmail.com>
2019-12-17 13:55:55 -0500

    Adding Links to Modules

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,42 @@
                     @endif
 
                     You are logged in!
+
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Users</h5>
+                                    <p class="card-text">Manage all your users in one place</p>
+                                    <a href="admin/users" class="btn btn-success btn-lg my-2">Manage</a>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Sales</h5>
+                                    <p class="card-text">We have special discounts at the best toys</p>
+                                    <a href="admin/users" class="btn btn-success btn-lg my-2">Manage</a>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Inventory</h5>
+                                    <p class="card-text">Manage with the click of a button</p>
+                                    <a href="admin/users" class="btn btn-success btn-lg my-2">Manage</a>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
                 </div>
             </div>
         </div>
