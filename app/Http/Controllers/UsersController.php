20f54a616e51870d145ea297fdc6eabefa52e9d6
Miguel Marquez <miguelxmarquez@gmail.com>
2019-12-17 11:11:43 -0500

    Fixing factory

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
+use Illuminate\Http\Request;
+use App\Models\User;
 class UsersController extends Controller
 {
     //
     public function index(){
-        return view('/admin/users/index');
+
+        $users = User::all();
+
+        return view('/admin/users/index')->with('users', $users);
     }
+
+
   
 }
