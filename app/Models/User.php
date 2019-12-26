09b845dc64d26f8a461b46ef4d33fe6dd4af8960
Miguel Márquez <miguelxmarquez@gmail.com>
2019-12-26 00:16:53 -0500

    Nitificable to User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 
 
 class User extends Authenticatable
 {
+    use Notifiable;
   
     protected $fillable = [
         'id', 'name', 'email', 'password', 'status', 'role'
