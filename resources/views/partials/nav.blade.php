81ebd30b881d22ee01c71fe02313f5258ac272fe
Miguel Márquez <miguelxmarquez@gmail.com>
2019-12-26 11:19:04 -0500

    Changing defaults routes

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,7 +4,7 @@
 @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
